ef6ba7fcc5750eabf3db886b2f1bf7b51e391670
Mahira <153006911+Walker71619@users.noreply.github.com>
2025-06-29 22:46:47 +0530

    Add files via upload

---
+++ HackathonPage.js
@@ -0,0 +1,14 @@
+// src/views/HackathonPage.js
+import React from 'react';
+import './HackathonPage.css'; // optional if you want styles
+
+const HackathonPage = () => {
+  return (
+    <div style={{ padding: '50px' }}>
+      <h1>Hackathons</h1>
+      <p>Welcome Hackers...</p>
+    </div>
+  );
+};
+
+export default HackathonPage;
