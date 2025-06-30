5fdc745d074f6cf5a6a6536e7fe598cb1c4785c8
Mahira <153006911+Walker71619@users.noreply.github.com>
2025-06-30 18:23:44 +0530

    Update HackathonPage.js

--- src/HackathonPage.js
+++ src/HackathonPage.js
@@ -1,12 +1,13 @@
-// src/views/HackathonPage.js
 import React from 'react';
-import './HackathonPage.css'; // optional if you want styles
+import './HackathonPage.css'; 
 
 const HackathonPage = () => {
   return (
-    <div style={{ padding: '50px' }}>
-      <h1>Hackathons</h1>
-      <p>Welcome Hackers...</p>
+    <div className="hackathon-page">
+      <div className="hackathon-content">
+        <h1>Hackathons</h1>
+        <p>Welcome Hackers...</p>
+      </div>
     </div>
   );
 };
