78fa664fd66cf3905cab5039e178cd836c46a970
Mahira <153006911+Walker71619@users.noreply.github.com>
2025-06-30 18:58:11 +0530

    Update HackathonPage.js

--- src/HackathonPage.js
+++ src/HackathonPage.js
@@ -1,10 +1,27 @@
 // src/views/HackathonPage.js
 import React from 'react';
 import './HackathonPage.css';
+import logo from '../assets/images/CL1.png'; // Make sure this path is correct
 
 const HackathonPage = () => {
   return (
     <div className="hackathon-page">
+      {/* Top Navigation Bar */}
+      <div className="top-strip">
+        <img src={logo} alt="Campus Link Logo" className="strip-logo" />
+        <ul className="strip-nav">
+          <li><a href="/">Home</a></li>
+          <li><a href="#">Freelance</a></li>
+          <li><a href="#">Hackathons</a></li>
+          <li><a href="#">Mentorship</a></li>
+          <li><a href="#">Events</a></li>
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#">Log In</a></li>
+          <li><a href="#" className="signup">Sign Up</a></li>
+        </ul>
+      </div>
+
+      {/* Hackathon Buttons */}
       <div className="hackathon-buttons">
         <button className="hack-btn">Organise a Hackathon</button>
         <button className="hack-btn">Participate as a Hacker</button>
