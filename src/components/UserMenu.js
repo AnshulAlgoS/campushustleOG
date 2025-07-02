0c226efa36a45ef20eddc803c6d0beef92c2cc09
Anshul Saxena <anshulsaxena9c6stc@gmail.com>
2025-07-02 17:01:05 +0530

    Create UserMenu.js

---
+++ src/components/UserMenu.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './UserMenu.css';
+
+export default function UserMenu({ user, onLogout, onProfileClick }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="user-menu" onClick={toggleDropdown}>
+      <div className="profile-icon" style={{ backgroundColor: '#fff', color: '#572E54' }}>
+        {user.email.charAt(0).toUpperCase()}
+      </div>
+      {isOpen && (
+        <div className="dropdown">
+          <p onClick={onProfileClick}>My Profile</p>
+          <button onClick={onLogout}>Logout</button>
+        </div>
+      )}
+    </div>
+  );
+}
