086aad6a580085dd98399cb73df2a42d235c3582
Anshul Saxena <anshulsaxena9c6stc@gmail.com>
2025-07-16 12:04:00 +0530

    Update FloatingDoodle.js

--- src/views/FloatingDoodle.js
+++ src/views/FloatingDoodle.js
@@ -4,7 +4,7 @@ import { Player } from '@lottiefiles/react-lottie-player';
 import Doodle1 from '../assets/images/Welcome-2.json';
 import Doodle2 from '../assets/images/Knowledge, Idea, Power, Books, Creativity, Learning Animation icon..json';
 import Doodle3 from '../assets/images/Rocket startup Animation.json';
-import Doodle4 from '../assets/images/Loading 40 | Paperplane.json';
+import Doodle4 from '../assets/images/Loading 40-Paperplane.json';
 import Doodle5 from '../assets/images/Girl with books.json';
 import Doodle6 from '../assets/images/Blob.json';
 import Doodle7 from '../assets/images/Run cycle recreated in Lottie Creator.json';
