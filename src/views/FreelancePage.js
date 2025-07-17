11255e9d5d3b7efdfc5da1578432deb6ef99a0e4
Mahira Khan <153006911+Walker71619@users.noreply.github.com>
2025-07-17 23:05:55 +0530

    Update FreelancePage.js

--- src/views/FreelancePage.js
+++ src/views/FreelancePage.js
@@ -2,21 +2,24 @@ import React from 'react';
 import './FreelancePage.css';
 import logo from '../assets/images/CL1.png';
 import { Link } from 'react-router-dom';
+import FloatingDoodles from './FloatingDoodle'; 
 
 const FreelancePage = () => {
   return (
     <div className="freelance-container">
-      <div className="navbar">
-        <img src={logo} alt="Campus Hustle Logo" className="logo" />
+      <FloatingDoodles /> {/* Animated doodles background */}
+
+      <div className="navbar updated-navbar">
+        <div className="nav-left">
+          <img src={logo} alt="Campus Hustle Logo" className="logo" />
+          <span className="brand-name">CampusHustle</span>
+        </div>
         <div className="nav-links">
           <Link to="/">Home</Link>
-          <Link to="/freelance">Freelance</Link>
-          <Link to="/hackathons">Hackathons</Link>
           <Link to="/mentorship">Mentorship</Link>
-          <Link to="/events">Events</Link>
-          <Link to="/dashboard">Dashboard</Link>
-          <Link to="/login">Log In</Link>
-          <Link to="/signup" className="signup-btn">Sign Up</Link>
+          <Link to="/hackathons">Hackathons</Link>
+          <Link to="/community">Community</Link>
+          <Link to="/about">About Us</Link>
         </div>
       </div>
 
@@ -24,10 +27,9 @@ const FreelancePage = () => {
         <Link to="/explore-freelance" className="freelance-btn">
           Explore Freelancing Opportunities
         </Link>
-       <Link to="/offer-work" className="freelance-btn">
-  Offer Work to Freelancers
-</Link>
-
+        <Link to="/offer-work" className="freelance-btn">
+          Offer Work to Freelancers
+        </Link>
       </div>
     </div>
   );
@@ -35,4 +37,3 @@ const FreelancePage = () => {
 
 export default FreelancePage;
 
-        
