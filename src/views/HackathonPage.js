d979f5a4604c71b86ce19da0cc9f097a23ecc04e
Mahira Khan <153006911+Walker71619@users.noreply.github.com>
2025-07-17 13:55:10 +0530

    Update HackathonPage.js

--- src/views/HackathonPage.js
+++ src/views/HackathonPage.js
@@ -1,39 +1,42 @@
 import React from 'react';
-import './HackathonPage.css';
-import logo from '../assets/images/CL1.png';
 import { Link } from 'react-router-dom';
+import FloatingDoodles from './FloatingDoodle';
+import logo from '../assets/images/CL1.png';
+import './HackathonPage.css';
 
 const HackathonPage = () => {
   return (
-    <div className="hackathon-page">
-      {/* Top Navigation Bar */}
-      <div className="translucent-strip">
-        <img src={logo} alt="Campus Link Logo" className="strip-logo" />
-        <ul className="strip-nav">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/freelance">Freelance</Link></li>
-          <li><Link to="/hackathons">Hackathons</Link></li>
-          <li><Link to="/mentorship">Mentorship</Link></li>
-          <li><Link to="/events">Events</Link></li>
-          <li><Link to="/dashboard">Dashboard</Link></li>
-          <li><Link to="/login">Log In</Link></li>
-          <li><Link to="/signup" className="signup">Sign Up</Link></li>
-        </ul>
-      </div>
+    <>
+      <FloatingDoodles /> {/* Animated doodles background */}
 
-      {/* Hackathon Buttons */}
-      <div className="hackathon-buttons">
-        <Link to="/organise-hackathon">
-          <button className="hack-btn">Organise a Hackathon</button>
-        </Link>
-        <Link to="/participation-guidelines">
-          <button className="hack-btn">Participation Guidelines</button>
-        </Link>
-        <Link to="/explore-hackathons">
-          <button className="hack-btn">Explore Hackathons</button>
-        </Link>
+      <div className="hackathon-page">
+        {/* Top Navigation Bar */}
+        <div className="translucent-strip">
+          <img src={logo} alt="Campus Hustle Logo" className="strip-logo" />
+          <ul className="strip-nav">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/freelance">Freelance</Link></li>
+            <li><Link to="/mentorship">Mentorship</Link></li>
+            <li><Link to="/community">Community</Link></li>
+            <li><Link to="/about">About Us</Link></li>
+            {/* Hackathons link removed because you're already on this page */}
+          </ul>
+        </div>
+
+        {/* Hackathon Buttons Section */}
+        <div className="hackathon-buttons">
+          <Link to="/organise-hackathon">
+            <button className="hack-btn">Organise a Hackathon</button>
+          </Link>
+          <Link to="/participation-guidelines">
+            <button className="hack-btn">Participation Guidelines</button>
+          </Link>
+          <Link to="/explore-hackathons">
+            <button className="hack-btn">Explore Hackathons</button>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
