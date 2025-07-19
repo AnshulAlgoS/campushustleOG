f51cab4c7b9e7e7b19a873bdb66cfff2a0af1079
Anshul Saxena <anshulsaxena9c6stc@gmail.com>
2025-07-19 14:47:08 +0530

    doodles updated

--- src/views/FloatingDoodle.js
+++ src/views/FloatingDoodle.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
+
 import Doodle1 from '../assets/images/Welcome-2.json';
 import Doodle2 from '../assets/images/Knowledge, Idea, Power, Books, Creativity, Learning Animation icon..json';
 import Doodle3 from '../assets/images/Rocket startup Animation.json';
@@ -8,21 +9,40 @@ import Doodle5 from '../assets/images/Girl with books.json';
 import Doodle6 from '../assets/images/Blob.json';
 import Doodle7 from '../assets/images/Run cycle recreated in Lottie Creator.json';
 
-const doodles = [
-  Doodle1, Doodle2, Doodle3, Doodle4, Doodle5, Doodle7, Doodle6,
-  Doodle1, Doodle3, Doodle5, Doodle2, Doodle7, Doodle6, Doodle4,
-  Doodle2, Doodle4, Doodle1, Doodle3, Doodle6, Doodle5, Doodle7,
+const doodleAnimations = [
+  Doodle1, Doodle2, Doodle3, Doodle4, Doodle5, Doodle6, Doodle7
 ];
 
 const FloatingDoodles = () => {
+  const doodles = useMemo(() =>
+    Array.from({ length: 20 }, () => {
+      const src = doodleAnimations[Math.floor(Math.random() * doodleAnimations.length)];
+      return {
+        src,
+        top: Math.random() * 100,
+        left: Math.random() * 100,
+        scale: Math.random() * 0.6 + 0.4,
+        rotate: Math.floor(Math.random() * 360)
+      };
+    }), []
+  );
+
   return (
     <div className="floating-doodles">
-      {doodles.map((src, index) => (
-        <div key={index} className={`doodle doodle-${index}`}>
+      {doodles.map((dot, index) => (
+        <div
+          key={index}
+          className="doodle"
+          style={{
+            top: `${dot.top}%`,
+            left: `${dot.left}%`,
+            transform: `translate(-50%, -50%) scale(${dot.scale}) rotate(${dot.rotate}deg)`
+          }}
+        >
           <Player
             autoplay
             loop
-            src={src}
+            src={dot.src}
             style={{ height: '60px', width: '60px' }}
           />
         </div>
