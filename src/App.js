f3e5432fdb8b0b1387883facdced574290df0ab4
Gourika <budhirajagourika8@gmail.com>
2025-07-29 12:33:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,6 +36,8 @@ import CommunityHub from './views/CommunityHub';
 import HelpCenter from './views/HelpCenter';
 import PrivacyPolicy from './views/PrivacyPolicy';
 import TermsAndConditions from './views/TermsAndConditions';
+import Contact from './views/Contact';
+
 
 
 function InnerApp() {
@@ -138,6 +140,8 @@ function InnerApp() {
         <Route path="/help-center" element={<HelpCenter />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms-and-conditions" element={<TermsAndConditions />} />
+        <Route path="/contact" element={<Contact />} />
+
       </Routes>
 
       {showAuthModal && (
