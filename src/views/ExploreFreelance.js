f38518e252409dfcd4d9fc212ff791d418b81fef
Anshul Saxena <anshulsaxena9c6stc@gmail.com>
2025-08-02 23:14:16 +0530

    UI updated

--- src/views/ExploreFreelance.js
+++ src/views/ExploreFreelance.js
@@ -23,16 +23,7 @@ const [menuOpen, setMenuOpen] = useState(false);
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/freelance">Freelance</Link></li>
                 <li><Link to="/hackathon">Hackathons</Link></li>
-                <li>
-                  <Link
-                    to="/"
-                    state={{ scrollTo: 'community' }}
-                    onClick={() => { }}
-                    className="desktop-link-btn"
-                  >
-                    Community
-                  </Link>
-                </li>
+                <li><Link to="/contact">Contact</Link></li>
 
                 <li><Link to="/about">About Us</Link></li>
                 <li>
