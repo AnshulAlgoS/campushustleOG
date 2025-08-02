8f655fb2b3314b94fa5a456990648b7842c3e559
Anshul Saxena <anshulsaxena9c6stc@gmail.com>
2025-08-03 01:49:46 +0530

    nav bar updated

--- src/views/HackathonPage.js
+++ src/views/HackathonPage.js
@@ -1,15 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import FloatingDoodles from './FloatingDoodle';
 import logo from '../assets/images/CL1.png';
 import './HackathonPage.css';
-import UserMenu from '../components/UserMenu';
 
-const HackathonPage = ({user, handleLogout, onProfileClick, openAuthModal}) => {
-      const [menuOpen, setMenuOpen] = useState(false);
+const HackathonPage = () => {
   return (
     <>
+
       <div className="hackathon-page">
+        {/* Top Navigation Bar */}
+        <div className="translucent-strip">
+           <div class="logo-container">
+          <img src={logo} alt="Campus Hustle Logo" className="strip-logo" />
+          <span class="logo-text">CampusHustle</span>
+  </div>
+          <ul className="strip-nav">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/freelance">Freelance</Link></li>
+            <li><Link to="/mentorship">Mentorship</Link></li>
+            <li><Link to="/community">Community</Link></li>
+            <li><Link to="/about">About Us</Link></li>
+          </ul>
+        </div>
+
         {/* Hackathon Buttons Section */}
         <div className="hackathon-buttons">
           <Link to="/organise-hackathon">
